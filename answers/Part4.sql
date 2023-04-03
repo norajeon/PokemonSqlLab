bcf40e035edda7c988d9e0f452244bbd692b7cf5
nora <sxj042@gmail.com>
2023-04-02 22:58:11 -0400

    finished

--- answers/Part4.sql
+++ answers/Part4.sql
@@ -1,17 +1,10 @@
-select pokemons.name, trainers.trainername, pokemon_trainer.pokelevel, pokemons.primary_type, pokemons.secondary_type
-from (((pokemon_trainer
-right outer join pokemons on pokemon_trainer.pokemon_id = pokemons.id)
-right outer join trainers on trainers.trainerID = pokemon_trainer.trainerID)
-right outer join types on pokemons.primary_type = types.id)
-ORDER BY pokemon_trainer.pokelevel DESC
-limit 10;
 
-
-select pokemons.name, trainers.trainername, pokemon_trainer.pokelevel, pokemons.primary_type, pokemons.secondary_type
-from (((pokemon_trainer
-right outer join pokemons on pokemon_trainer.pokemon_id = pokemons.id)
-inner join trainers on trainers.trainerID = pokemon_trainer.trainerID)
-inner join types on pokemons.primary_type = types.id)
+SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel,
+a.name AS primary_type, b.name AS secondary_type
+FROM pokemons
+INNER JOIN types AS a ON primary_type = a.id
+INNER JOIN types AS b ON secondary_type = b.id
+INNER JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
+INNER JOIN trainers ON trainers.trainerID = pokemon_trainer.trainerID
 ORDER BY pokemon_trainer.pokelevel DESC
-limit 10;
 
